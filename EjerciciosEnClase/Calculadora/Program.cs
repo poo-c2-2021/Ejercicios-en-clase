8b82fb3082b77d37d4e84e22b859076d33cc7c2d
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2021-08-25 23:28:58 -0300

    commit to main

---
+++ EjerciciosEnClase/Calculadora/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeritoString;
+            int num1 = 2;
+            int num2 = 5;
+            int resultado;
+
+            Console.WriteLine("Ingrese un numero");
+
+            numeritoString = Console.ReadLine();
+
+            while (!int.TryParse(numeritoString, out num1))
+            {
+                Console.WriteLine("ERROR,Ingrese un numero");
+                numeritoString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Ingrese otro numero");
+
+            while (!int.TryParse(Console.ReadLine(), out num2))
+                Console.WriteLine("ERROR,Ingrese un numero");
+
+            resultado = num1 + num2;
+
+            Console.WriteLine($"El resultado es {resultado}");
+
+
+        }
+    }
+}
---
+++ EjerciciosEnClase/EjemploArrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EjemploArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayNombres;
+            int[] numero = new int[1];
+
+            numero[0] = 1;
+
+            arrayNombres = new string[5];
+
+            arrayNombres[0] = "Pepe";
+            arrayNombres[1] = "Juana";
+            arrayNombres[2] = "Romeo";
+            arrayNombres[3] = "Betun";
+            arrayNombres[4] = "Juanito";
+
+            ModificarArray(arrayNombres);
+
+            MostrarArray(arrayNombres);
+        }
+
+
+        static void ModificarArray(string[] arrayAux)
+        {
+            arrayAux[0] = "Lucas";
+
+        }
+
+
+        static void MostrarArray(string[] arrayAux)
+        {
+
+            for (int i = 0; i < arrayAux.Length; i++)
+            {
+                Console.WriteLine(arrayAux[i]);
+            }
+
+        }
+
+    }
+}
