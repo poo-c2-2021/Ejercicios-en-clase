5b44106230ce1defeeb8fa9a344f67bf144993ee
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2021-08-27 23:43:47 -0300

    ejercicio 2

---
+++ EjerciciosEnClase/02_Tarea_Ejercicio_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Tarea_Ejercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
