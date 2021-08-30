be3cd9921a23a91d744ead376bc2150398b75465
Caro Scrofani <caro.scrofani@gmail.com>
2021-08-30 15:40:05 -0300

    ejercicio de tarea 02

--- EjerciciosEnClase/02_Tarea_Ejercicio_02/Program.cs
+++ EjerciciosEnClase/02_Tarea_Ejercicio_02/Program.cs
@@ -6,7 +6,13 @@ namespace _02_T_Ej_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona primeraPersona = new Persona("Lucas", 32, 30666666);
+            Persona segundaPersona = new Persona("Ezequiel", 35, 32666666);
+            Persona terceraPersona = new Persona("Juampi", 12, 40666666);
+
+            primeraPersona.Mostrar();
+            segundaPersona.Mostrar();
+            terceraPersona.Mostrar();
         }
     }
 }
