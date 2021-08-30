e8ea5705e2d5560e7dd860bea9cd4ea3e8c04e01
Caro Scrofani <caro.scrofani@gmail.com>
2021-08-30 14:38:39 -0300

    tarea ejercicio 04

---
+++ EjerciciosEnClase/02_T_Ej_04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_T_Ej_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
