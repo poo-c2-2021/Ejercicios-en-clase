e1aa13c268b9efc0120ba577519d9c61ed220ebc
Caro Scrofani <caro.scrofani@gmail.com>
2021-08-30 16:34:06 -0300

    ejercicio de tarea 04

---
+++ EjerciciosEnClase/02_T_Ej_04/Conductor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_T_Ej_04
+{
+    public class Conductor
+    {
+        string nombre;
+        int[] kms;
+
+        public Conductor(string nombre, int[] kms)
+        {
+            this.nombre = nombre;
+            this.kms = kms;
+        }
+
+        public double KmTotales()
+        {
+            double aux=0;
+            for (int i = 0; i < kms.Length; i++)
+            {
+                aux += kms[i];
+            }
+            return aux;
+        }
+
+        public int GetDia3()
+        {
+            return kms[2];
+        }
+        
+        public int GetDia5()
+        {
+            return kms[4];
+        }
+
+        public string MostrarNombre()
+        {
+          return this.nombre;
+        }
+    }
+}
--- EjerciciosEnClase/02_T_Ej_04/Program.cs
+++ EjerciciosEnClase/02_T_Ej_04/Program.cs
@@ -6,7 +6,38 @@ namespace _02_T_Ej_04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Conductor conductor1 = new Conductor("Lucas", new int[] { 200, 599, 899, 0, 256, 0, 0});
+            Conductor conductor2 = new Conductor("Eze", new int[] { 400, 500, 900, 100, 250, 0, 0});
+            Conductor conductor3 = new Conductor("Juampi", new int[] { 100, 300, 860, 200, 260, 70, 800});
+
+            Conductor auxiliar;
+
+            if (conductor1.GetDia3() < conductor2.GetDia3())
+            {
+                if (conductor2.GetDia3() < conductor3.GetDia3())
+                {
+                    auxiliar = conductor3;
+                }
+                else
+                {
+                    auxiliar = conductor2;
+                }
+            }
+            else 
+            {
+
+                if (conductor1.GetDia3() < conductor3.GetDia3())
+                {
+                    auxiliar = conductor3;
+                }
+                else
+                {
+                    auxiliar = conductor1;
+                }
+            }
+
+            Console.WriteLine($"El que manejó más el día 3: {auxiliar.MostrarNombre()}");
+
         }
     }
 }
