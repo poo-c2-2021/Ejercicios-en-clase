b00e6b9e0ccee9b71a22cad33db6463c8fb685b0
Caro Scrofani <caro.scrofani@gmail.com>
2021-08-30 16:06:15 -0300

    ejercicio de tarea 03

--- EjerciciosEnClase/02_Tarea_Ejercicio_03/Program.cs
+++ EjerciciosEnClase/02_Tarea_Ejercicio_03/Program.cs
@@ -10,6 +10,15 @@ namespace _02_T_Ej_03
     {
         static void Main(string[] args)
         {
+            Cuenta cuenta1 = new Cuenta("Eze", 150000);
+
+            cuenta1.Mostrar();
+            cuenta1.Ingresar(600);
+            cuenta1.Mostrar();
+            cuenta1.Retirar(300);
+            cuenta1.Mostrar();
+            Console.ReadKey();
+
         }
     }
 }
